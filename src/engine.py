3d7165ce610dee0c290fa7f1e194b511b0a5b3da
Sergey KSV <sergey@tlight.biz>
2026-04-01 12:18:43 +0300

    xx

--- src/engine.py
+++ src/engine.py
@@ -487,7 +487,8 @@ def run_workers_thread(app_settings):
 
         insert_storage = insert_buffer()
 
-        USER_FUNC.initiate_dropdown_workers()
+        if STAGE == Stage.SYNCH:
+            USER_FUNC.initiate_dropdown_workers()
 
         logger.info(f"launch worker threads")
 
@@ -512,7 +513,7 @@ def run_workers_thread(app_settings):
                 values = [p.values for p in rows]
                 try:
                     USER_FUNC.dump_values(rows[0].table_name, columns, values)
-                    if STAGE == Stage.REGENERATION:
+                    if STAGE in [Stage.REGENERATION, Stage.REGENERATION_DUMP_DONE]:
                         REGENERATION_CONTROLLER.add_parsed_count(rows[0].table_name, len(values))
                 except Exception as e:
                     logger.exception(e)
